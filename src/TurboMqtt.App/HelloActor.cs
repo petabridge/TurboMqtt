ef2757a0453775bbba298f0507de1cdd3b1412d5
Aaron Stannard <aaron@petabridge.com>
2024-04-12 10:28:55 -0500

    initial commit

---
+++ src/TurboMqtt.App/HelloActor.cs
@@ -0,0 +1,15 @@
+﻿namespace TurboMqtt.App;
+
+public class HelloActor : ReceiveActor
+{
+    private readonly ILoggingAdapter _log = Context.GetLogger();
+    private int _helloCounter = 0;
+    
+    public HelloActor()
+    {
+        Receive<string>(message =>
+        {
+           _log.Info("{0} {1}", message, _helloCounter++);
+        });
+    }
+}
