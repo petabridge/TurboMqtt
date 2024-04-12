6922e27944694037a6102bdf1147ec6b950911d6
Aaron Stannard <aaron@petabridge.com>
2024-04-12 11:07:28 -0500

    adding MQTT packet control headers and QoS values

---
+++ src/TurboMqtt.Core/ControlPacketHeaders.cs
@@ -0,0 +1,36 @@
+namespace TurboMqtt.Core;
+
+/// <summary>
+/// The type of MQTT packet.
+/// </summary>
+/// <remarks>
+/// Aligns to the MQTT 3.1.1 specification: https://docs.oasis-open.org/mqtt/mqtt/v3.1.1/os/mqtt-v3.1.1-os.html
+///
+/// Also supports MQTT 5.0.
+/// </remarks>
+public enum MqttPacketType
+{
+    Connect = 0x10,
+    ConnAck = 0x20,
+    Publish = 0x30,
+    PubAck = 0x40,
+    PubRec = 0x50,
+    PubRel = 0x60,
+    PubComp = 0x70,
+    Subscribe = 0x80,
+    SubAck = 0x90,
+    Unsubscribe = 0xA0,
+    UnsubAck = 0xB0,
+    PingReq = 0xC0,
+    PingResp = 0xD0,
+    Disconnect = 0xE0,
+    // Reserved = 0xF0, // Normally reserved values are not included in enums
+}
+
+public static class MqttHelpers
+{
+    public static byte GetControlPacketType(MqttPacketType packetType)
+    {
+        return (byte)packetType;
+    }
+}
---
+++ src/TurboMqtt.Core/QualityOfService.cs
@@ -0,0 +1,8 @@
+namespace TurboMqtt.Core;
+
+public enum QualityOfService
+{
+    AtMostOnce = 0,
+    AtLeastOnce = 1,
+    ExactlyOnce = 2
+}
