7c71ed07f1b5ff6cc8186c93ea7ab4fb18f6e86a
Aaron Stannard <aaron@petabridge.com>
2024-04-14 15:18:26 -0500

    finished covering all of the Publish-related packets

---
+++ tests/TurboMqtt.Core.Tests/Packets/PubPackets/PubCompPacketMqtt311EndToEndCodecSpecs.cs
@@ -0,0 +1,38 @@
+// -----------------------------------------------------------------------
+// <copyright file="PubCompPacketMqtt311EndToEndCodecSpecs.cs" company="Petabridge, LLC">
+//      Copyright (C) 2024 - 2024 Petabridge, LLC <https://petabridge.com>
+// </copyright>
+// -----------------------------------------------------------------------
+
+using TurboMqtt.Core.PacketTypes;
+using TurboMqtt.Core.Protocol;
+
+namespace TurboMqtt.Core.Tests.Packets.PubPackets;
+
+public class PubCompPacketMqtt311EndToEndCodecSpecs
+{
+    public class SingleMessage
+    {
+        private readonly Mqtt311Decoder _decoder = new();
+
+        public static readonly TheoryData<PubCompPacket> PubCompPackets = new()
+        {
+            new PubCompPacket
+            {
+                PacketId = 1
+            },
+            new PubCompPacket
+            {
+                PacketId = 2
+            },
+        };
+
+        [Theory]
+        [MemberData(nameof(PubCompPackets))]
+        public void ShouldEncodeAndDecodeCorrectly(PubCompPacket packet)
+        {
+            var decodedPacket = PacketEncodingTestHelper.EncodeAndDecodeMqtt311Packet(packet, _decoder);
+            decodedPacket.PacketId.Should().Be(packet.PacketId);
+        }
+    }
+}
