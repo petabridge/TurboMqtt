1ca6e67805a5cebc90a5eb4e1a0d5d46186de11b
Aaron Stannard <aaron@petabridge.com>
2024-04-14 17:50:41 -0500

    added initial benchmarks (#10)

---
+++ benchmarks/TurboMqtt.Core.Benchmarks/MicroBenchmarkConfig.cs
@@ -0,0 +1,24 @@
+// -----------------------------------------------------------------------
+// <copyright file="MicroBenchmarkConfigh.cs" company="Petabridge, LLC">
+//      Copyright (C) 2024 - 2024 Petabridge, LLC <https://petabridge.com>
+// </copyright>
+// -----------------------------------------------------------------------
+
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Exporters;
+
+namespace TurboMqtt.Core.Benchmarks;
+
+/// <summary>
+/// Basic BenchmarkDotNet configuration used for microbenchmarks.
+/// </summary>
+public class MicroBenchmarkConfig : ManualConfig
+{
+    public MicroBenchmarkConfig()
+    {
+        AddDiagnoser(MemoryDiagnoser.Default);
+        AddExporter(MarkdownExporter.GitHub);
+        //AddColumn(new RequestsPerSecondColumn());
+    }
+}
