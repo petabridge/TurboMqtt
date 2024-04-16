de019306a2c2bcb34d03a88e823c7b887a08cd45
Aaron Stannard <aaron@petabridge.com>
2024-04-16 16:43:25 -0500

    added tests to cover all scenarios for atleastoncedelivery

--- tests/TurboMqtt.Core.Tests/Protocol/AtLeastOncePublishRetryActorSpecs.cs
+++ tests/TurboMqtt.Core.Tests/Protocol/AtLeastOncePublishRetryActorSpecs.cs
@@ -49,4 +49,53 @@ public class AtLeastOncePublishRetryActorSpecs : TestKit
         
         probe.ExpectMsg<PublishingProtocol.PublishFailure>().Reason.Should().Be("Duplicate packet ID");
     }
+
+    [Fact] public async Task AtLeastOncePublishRetryActor_should_resend_overdue_publish_packets()
+    {
+        var probe = CreateTestProbe();
+        var channel = Channel.CreateUnbounded<MqttPacket>();
+        
+        // set the timespan to zero, so we have to immediately retry
+        var actor = Sys.ActorOf(Props.Create(() =>
+            new AtLeastOncePublishRetryActor(channel.Writer, 3, TimeSpan.Zero)));
+
+        var packet = new PublishPacket(QualityOfService.AtLeastOnce, false, false, "topic")
+        {
+            PacketId = 1
+        };
+        
+        actor.Tell(packet, probe);
+        actor.Tell(PublishProtocolDefaults.CheckPublishTimeout.Instance);
+        
+        // we should have received the packet back
+        using var cts = new CancellationTokenSource(RemainingOrDefault);
+        var result = await channel.Reader.ReadAsync(cts.Token);
+        result.Should().Be(packet);
+        result.Duplicate.Should().BeTrue(); // duplicate flag needs to be set on retries
+        
+        // ack the packet
+        var pubAck = packet.ToPubAck();
+        actor.Tell(pubAck, probe);
+        await probe.ExpectMsgAsync<PublishingProtocol.PublishSuccess>(cancellationToken: cts.Token);
+    }
+    
+    [Fact] public async Task AtLeastOncePublishRetryActor_should_fail_undeliverable_packets()
+    {
+        var probe = CreateTestProbe();
+        var channel = Channel.CreateUnbounded<MqttPacket>();
+        
+        // set the timespan to zero, so we have to immediately retry - no retries available, so we'll immediately fail
+        var actor = Sys.ActorOf(Props.Create(() =>
+            new AtLeastOncePublishRetryActor(channel.Writer, 0, TimeSpan.Zero)));
+
+        var packet = new PublishPacket(QualityOfService.AtLeastOnce, false, false, "topic")
+        {
+            PacketId = 1
+        };
+        
+        actor.Tell(packet, probe);
+        actor.Tell(PublishProtocolDefaults.CheckPublishTimeout.Instance);
+        // should get a failure message
+        await probe.ExpectMsgAsync<PublishingProtocol.PublishFailure>();
+    }
 }
