907aad0ed58552ad88b21570179e6a40f356d9d0
Aaron Stannard <aaron@petabridge.com>
2024-04-18 16:49:22 -0500

    getting ready to define some MSFT EXT integration

---
+++ src/TurboMqtt.Core/TurbotMqttHostingExtensions.cs
@@ -0,0 +1,17 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="TurbotMqttHostingExtensions.cs" company="Petabridge, LLC">
+//      Copyright (C) 2024 - 2024 Petabridge, LLC <https://petabridge.com>
+// </copyright>
+// -----------------------------------------------------------------------
+
+using Akka.Actor;
+
+namespace TurboMqtt.Core;
+
+/// <summary>
+/// Used to tie into the Akka.NET <see cref="ActorSystem"/> and start up the TurboMqtt server.
+/// </summary>
+public static class TurbotMqttHostingExtensions
+{
+    public static IServiceCollection 
+}
