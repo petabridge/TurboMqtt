5f2accb6bfc48f442694d2326ecdd7ebfc62ce1a
Aaron Stannard <aaron@petabridge.com>
2024-04-19 11:53:35 -0500

    added automatic process exit
    still need to debug why the samples hang when we can't connect

--- samples/TurboMqtt.Samples.BackpressureProducer/MqttProducerService.cs
+++ samples/TurboMqtt.Samples.BackpressureProducer/MqttProducerService.cs
@@ -19,48 +19,60 @@ public sealed class MqttProducerService : BackgroundService
     private readonly ILogger<MqttProducerService> _logger;
     private readonly IMqttClientFactory _clientFactory;
     private readonly IOptionsSnapshot<MqttConfig> _config;
+    private readonly IHostLifetime _lifetime;
 
-    public MqttProducerService(IMqttClientFactory clientFactory, IOptionsSnapshot<MqttConfig> config, ILogger<MqttProducerService> logger)
+    public MqttProducerService(IMqttClientFactory clientFactory, 
+        IOptionsSnapshot<MqttConfig> config, 
+        ILogger<MqttProducerService> logger, IHostLifetime lifetime)
     {
         _clientFactory = clientFactory;
         _config = config;
         _logger = logger;
+        _lifetime = lifetime;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        var config = _config.Value;
-        
-        var tcpClientOptions = new MqttClientTcpOptions(config.Host, config.Port);
-        var clientConnectOptions = new MqttClientConnectOptions(config.ClientId, MqttProtocolVersion.V3_1_1)
+        try
         {
-            UserName = config.User,
-            Password = config.Password
-        };
+            var config = _config.Value;
         
-        var client = await _clientFactory.CreateTcpClient(clientConnectOptions, tcpClientOptions);
-        using var connectCts = new CancellationTokenSource(TimeSpan.FromSeconds(5));
-        var linkedCts = CancellationTokenSource.CreateLinkedTokenSource(connectCts.Token, stoppingToken);
-        var connectResult = await client.ConnectAsync(linkedCts.Token);
-        if(!connectResult.IsSuccess)
-        {
-            _logger.LogError("Failed to connect to MQTT broker at {0}:{1} - {2}", config.Host, config.Port, connectResult.Reason);
-            return;
-        }
-        
-        _logger.LogInformation("Connected to MQTT broker at {0}:{1}", config.Host, config.Port);
-        foreach(var i in Enumerable.Range(0, config.MessageCount))
-        {
-            var msg = new MqttMessage(config.Topic, $"msg-{i}");
-            
-            await client.PublishAsync(msg, stoppingToken);
-            if(i % 1000 == 0)
+            var tcpClientOptions = new MqttClientTcpOptions(config.Host, config.Port);
+            var clientConnectOptions = new MqttClientConnectOptions(config.ClientId, MqttProtocolVersion.V3_1_1)
             {
-                _logger.LogInformation("Published {0} messages", i);
+                UserName = config.User,
+                Password = config.Password
+            };
+
+            await using var client = await _clientFactory.CreateTcpClient(clientConnectOptions, tcpClientOptions);
+            using var connectCts = new CancellationTokenSource(TimeSpan.FromSeconds(5));
+            var linkedCts = CancellationTokenSource.CreateLinkedTokenSource(connectCts.Token, stoppingToken);
+            var connectResult = await client.ConnectAsync(linkedCts.Token);
+            if(!connectResult.IsSuccess)
+            {
+                _logger.LogError("Failed to connect to MQTT broker at {0}:{1} - {2}", config.Host, config.Port, connectResult.Reason);
+                return;
             }
-        }
         
-        _logger.LogInformation("Shutting down MQTT consumer service");
-        await client.DisposeAsync();
+            _logger.LogInformation("Connected to MQTT broker at {0}:{1}", config.Host, config.Port);
+            foreach(var i in Enumerable.Range(0, config.MessageCount))
+            {
+                var msg = new MqttMessage(config.Topic, $"msg-{i}");
+            
+                await client.PublishAsync(msg, stoppingToken);
+                //if(i % 1000 == 0)
+                {
+                    _logger.LogInformation("Published {0} messages", i);
+                }
+            }
+        
+            _logger.LogInformation("Shutting down MQTT consumer service");
+            await client.DisconnectAsync(stoppingToken);
+        }
+        catch(Exception ex)
+        {
+            _logger.LogError(ex, "Error occurred in MQTT producer service");
+            _ = _lifetime.StopAsync(default);
+        }
     }
 }
--- samples/TurboMqtt.Samples.DevNullConsumer/MqttConsumerService.cs
+++ samples/TurboMqtt.Samples.DevNullConsumer/MqttConsumerService.cs
@@ -18,59 +18,72 @@ public sealed class MqttConsumerService : BackgroundService
     private readonly ILogger<MqttConsumerService> _logger;
     private readonly IMqttClientFactory _clientFactory;
     private readonly IOptionsSnapshot<MqttConfig> _config;
+    private readonly IHostLifetime _lifetime;
 
-    public MqttConsumerService(IMqttClientFactory clientFactory, IOptionsSnapshot<MqttConfig> config, ILogger<MqttConsumerService> logger)
+    public MqttConsumerService(IMqttClientFactory clientFactory, IOptionsSnapshot<MqttConfig> config,
+        ILogger<MqttConsumerService> logger, IHostLifetime lifetime)
     {
         _clientFactory = clientFactory;
         _config = config;
         _logger = logger;
+        _lifetime = lifetime;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        var config = _config.Value;
-        
-        var tcpClientOptions = new MqttClientTcpOptions(config.Host, config.Port);
-        var clientConnectOptions = new MqttClientConnectOptions(config.ClientId, MqttProtocolVersion.V3_1_1)
+        try
         {
-            UserName = config.User,
-            Password = config.Password
-        };
-        
-        var client = await _clientFactory.CreateTcpClient(clientConnectOptions, tcpClientOptions);
-        using var connectCts = new CancellationTokenSource(TimeSpan.FromSeconds(5));
-        var linkedCts = CancellationTokenSource.CreateLinkedTokenSource(connectCts.Token, stoppingToken);
-        var connectResult = await client.ConnectAsync(linkedCts.Token);
-        if(!connectResult.IsSuccess)
-        {
-            _logger.LogError("Failed to connect to MQTT broker at {0}:{1} - {2}", config.Host, config.Port, connectResult.Reason);
-            return;
-        }
-        
-        _logger.LogInformation("Connected to MQTT broker at {0}:{1}", config.Host, config.Port);
-        var subscribeResult = await client.SubscribeAsync(config.Topic, config.QoS, linkedCts.Token);
-        if (!subscribeResult.IsSuccess)
-        {
-            _logger.LogError("Failed to subscribe to topic {0} - {1}", config.Topic, subscribeResult.Reason);
-            return;
-        }
+            var config = _config.Value;
 
-        _logger.LogInformation("Subscribed to topic {0}", config.Topic);
-
-        var received = 0;
-        var receivedMessage = client.ReceivedMessages;
-        while(await receivedMessage.WaitToReadAsync(stoppingToken))
-        {
-            while(receivedMessage.TryRead(out _))
+            var tcpClientOptions = new MqttClientTcpOptions(config.Host, config.Port);
+            var clientConnectOptions = new MqttClientConnectOptions(config.ClientId, MqttProtocolVersion.V3_1_1)
             {
-                if (++received % 1000 == 0)
+                UserName = config.User,
+                Password = config.Password
+            };
+
+            var client = await _clientFactory.CreateTcpClient(clientConnectOptions, tcpClientOptions);
+            using var connectCts = new CancellationTokenSource(TimeSpan.FromSeconds(5));
+            var linkedCts = CancellationTokenSource.CreateLinkedTokenSource(connectCts.Token, stoppingToken);
+            var connectResult = await client.ConnectAsync(linkedCts.Token);
+            if (!connectResult.IsSuccess)
+            {
+                _logger.LogError("Failed to connect to MQTT broker at {0}:{1} - {2}", config.Host, config.Port,
+                    connectResult.Reason);
+                return;
+            }
+
+            _logger.LogInformation("Connected to MQTT broker at {0}:{1}", config.Host, config.Port);
+            var subscribeResult = await client.SubscribeAsync(config.Topic, config.QoS, linkedCts.Token);
+            if (!subscribeResult.IsSuccess)
+            {
+                _logger.LogError("Failed to subscribe to topic {0} - {1}", config.Topic, subscribeResult.Reason);
+                return;
+            }
+
+            _logger.LogInformation("Subscribed to topic {0}", config.Topic);
+
+            var received = 0;
+            var receivedMessage = client.ReceivedMessages;
+            while (await receivedMessage.WaitToReadAsync(stoppingToken))
+            {
+                while (receivedMessage.TryRead(out _))
                 {
-                    _logger.LogInformation("Received {0} messages", received);
+                    if (++received % 1000 == 0)
+                    {
+                        _logger.LogInformation("Received {0} messages", received);
+                    }
                 }
             }
+
+            _logger.LogInformation("Shutting down MQTT consumer service");
+            await client.DisposeAsync();
         }
-        
-        _logger.LogInformation("Shutting down MQTT consumer service");
-        await client.DisposeAsync();
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error occurred in MQTT consumer service");
+            // shut the process down
+            _ = _lifetime.StopAsync(default);
+        }
     }
 }
