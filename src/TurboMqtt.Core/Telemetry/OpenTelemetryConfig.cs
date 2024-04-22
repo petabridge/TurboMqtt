b5188b3308384380c6c3cbcb3c41d61e1c41271e
Aaron Stannard <aaron@petabridge.com>
2024-04-22 10:16:04 -0500

    OTEL sources for TurboMqtt

---
+++ src/TurboMqtt.Core/Telemetry/OpenTelemetryConfig.cs
@@ -0,0 +1,21 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="OpenTelemetryConfig.cs" company="Petabridge, LLC">
+//      Copyright (C) 2024 - 2024 Petabridge, LLC <https://petabridge.com>
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System.Diagnostics;
+using System.Diagnostics.Metrics;
+using TurboMqtt.Core.Client;
+
+namespace TurboMqtt.Core.Telemetry;
+
+internal static class OpenTelemetryConfig
+{
+    private static readonly string
+        Version = typeof(IMqttClient).Assembly.GetName().Version?.ToString() ?? string.Empty;
+    
+    public static readonly ActivitySource ActivitySource = new("TurboMqtt", Version);
+    
+    public static readonly Meter Meter = new Meter("TurboMqtt", Version);
+}
