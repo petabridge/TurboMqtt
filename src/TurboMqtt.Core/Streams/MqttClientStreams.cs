9c082c711eb7a5ea153fe0a08b4c632d6e99b6a6
Aaron Stannard <aaron@petabridge.com>
2024-04-22 11:56:58 -0500

    make it possible to opt out of telemetry

--- src/TurboMqtt.Core/Streams/MqttClientStreams.cs
+++ src/TurboMqtt.Core/Streams/MqttClientStreams.cs
@@ -21,30 +21,44 @@ namespace TurboMqtt.Core.Streams;
 internal static class MqttClientStreams
 {
     public static Sink<MqttPacket, NotUsed> Mqtt311OutboundPacketSink(string clientId, IMqttTransport transport,
-        MemoryPool<byte> memoryPool, int maxFrameSize, int maxPacketSize)
+        MemoryPool<byte> memoryPool, int maxFrameSize, int maxPacketSize, bool withTelemetry = true)
     {
         var finalSink = Sink.FromWriter(transport.Writer, true);
-        var g = Flow.Create<MqttPacket>()
-            .Via(OpenTelemetryFlows.MqttPacketRateTelemetryFlow(MqttProtocolVersion.V3_1_1, clientId, OpenTelemetrySupport.Direction.Outbound))
-            .Via(MqttEncodingFlows.Mqtt311Encoding(memoryPool, maxFrameSize, maxPacketSize))
-            .Via(OpenTelemetryFlows.MqttBitRateTelemetryFlow(MqttProtocolVersion.V3_1_1, clientId, OpenTelemetrySupport.Direction.Outbound))
-            .To(finalSink);
+        if (withTelemetry)
+            return Flow.Create<MqttPacket>()
+                .Via(OpenTelemetryFlows.MqttPacketRateTelemetryFlow(MqttProtocolVersion.V3_1_1, clientId,
+                    OpenTelemetrySupport.Direction.Outbound))
+                .Via(MqttEncodingFlows.Mqtt311Encoding(memoryPool, maxFrameSize, maxPacketSize))
+                .Via(OpenTelemetryFlows.MqttBitRateTelemetryFlow(MqttProtocolVersion.V3_1_1, clientId,
+                    OpenTelemetrySupport.Direction.Outbound))
+                .To(finalSink);
 
-        return g;
+        return Flow.Create<MqttPacket>()
+            .Via(MqttEncodingFlows.Mqtt311Encoding(memoryPool, maxFrameSize, maxPacketSize))
+            .To(finalSink);
     }
 
-    public static Source<MqttMessage, NotUsed> Mqtt311InboundMessageSource(string clientId, IMqttTransport transport, 
+    public static Source<MqttMessage, NotUsed> Mqtt311InboundMessageSource(string clientId, IMqttTransport transport,
         ChannelWriter<MqttPacket> outboundPackets,
-        MqttRequiredActors actors, int maxRememberedPacketIds, TimeSpan packetIdExpiry)
+        MqttRequiredActors actors, int maxRememberedPacketIds, TimeSpan packetIdExpiry, bool withTelemetry = true)
+
     {
-        var finalSource = (ChannelSource.FromReader(transport.Reader)
-            .Via(OpenTelemetryFlows.MqttBitRateTelemetryFlow(MqttProtocolVersion.V3_1_1, clientId, OpenTelemetrySupport.Direction.Inbound))
+        if (withTelemetry)
+            return (ChannelSource.FromReader(transport.Reader)
+                .Via(OpenTelemetryFlows.MqttBitRateTelemetryFlow(MqttProtocolVersion.V3_1_1, clientId,
+                    OpenTelemetrySupport.Direction.Inbound))
+                .Via(MqttDecodingFlows.Mqtt311Decoding())
+                .Via(OpenTelemetryFlows.MqttPacketRateTelemetryFlow(MqttProtocolVersion.V3_1_1, clientId,
+                    OpenTelemetrySupport.Direction.Inbound))
+                .Via(MqttReceiverFlows.ClientAckingFlow(maxRememberedPacketIds, packetIdExpiry, outboundPackets,
+                    actors))
+                .Where(c => c.PacketType == MqttPacketType.Publish)
+                .Select(c => ((PublishPacket)c).FromPacket()));
+
+        return (ChannelSource.FromReader(transport.Reader)
             .Via(MqttDecodingFlows.Mqtt311Decoding())
-            .Via(OpenTelemetryFlows.MqttPacketRateTelemetryFlow(MqttProtocolVersion.V3_1_1, clientId, OpenTelemetrySupport.Direction.Inbound))
             .Via(MqttReceiverFlows.ClientAckingFlow(maxRememberedPacketIds, packetIdExpiry, outboundPackets, actors))
             .Where(c => c.PacketType == MqttPacketType.Publish)
             .Select(c => ((PublishPacket)c).FromPacket()));
-
-        return finalSource;
     }
 }
