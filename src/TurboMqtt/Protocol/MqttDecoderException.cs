9993a6f7edde56f3120e271fa7c6e9580f6dd1e0
Aaron Stannard <aaron@petabridge.com>
2024-04-30 16:34:12 -0500

    [WIP] better error reporting for decoder errors  (#84)
    * Added `MqttDecoderException` type
    
    Working on making decoding exceptions easier to diagnose
    
    * added more useful data when a decoding error gets found

---
+++ src/TurboMqtt/Protocol/MqttDecoderException.cs
@@ -0,0 +1,38 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="MqttDecoderException.cs" company="Petabridge, LLC">
+//      Copyright (C) 2024 - 2024 Petabridge, LLC <https://petabridge.com>
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace TurboMqtt.Protocol;
+
+/// <summary>
+/// Thrown when the decoder encounters an error while parsing the MQTT packet.
+/// </summary>
+public sealed class MqttDecoderException : Exception
+{
+    public MqttPacketType? PacketType { get; }
+    public MqttProtocolVersion ProtocolVersion { get; }
+
+    public MqttDecoderException(string message, MqttProtocolVersion protocolVersion, MqttPacketType? packetType = null)
+        : base(message)
+    {
+        ProtocolVersion = protocolVersion;
+        PacketType = packetType;
+    }
+
+    public MqttDecoderException(string message, Exception innerEx, MqttProtocolVersion protocolVersion,
+        MqttPacketType? packetType = null) : base(message,
+        innerException: innerEx)
+    {
+        ProtocolVersion = protocolVersion;
+        PacketType = packetType;
+    }
+
+    public override string ToString()
+    {
+        // check if there's an InnerException and log that too
+        return $"MqttDecoderException[ProtocolVersion={ProtocolVersion}, PacketType={PacketType}]" +
+               Environment.NewLine + base.ToString();
+    }
+}
