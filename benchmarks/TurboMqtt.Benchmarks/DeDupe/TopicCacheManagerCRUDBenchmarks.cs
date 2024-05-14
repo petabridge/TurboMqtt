bef8733eb253c2b36ebc898f08e97f8ef983181e
Aaron Stannard <aaron@petabridge.com>
2024-05-14 16:55:32 -0500

    categorized benchmarks

--- benchmarks/TurboMqtt.Benchmarks/DeDupe/TopicCacheManagerCRUDBenchmarks.cs
+++ benchmarks/TurboMqtt.Benchmarks/DeDupe/TopicCacheManagerCRUDBenchmarks.cs
@@ -5,10 +5,13 @@
 // -----------------------------------------------------------------------
 
 using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
 using TurboMqtt.Utility;
 
 namespace TurboMqtt.Benchmarks.DeDupe;
 
+[GroupBenchmarksBy(BenchmarkLogicalGroupRule.ByCategory)]
+[CategoriesColumn]
 public class TopicCacheManagerCrudBenchmarks
 {
     [Params(1,2,3)] public int ActiveTopics { get; set; }
@@ -20,31 +23,72 @@ public class TopicCacheManagerCrudBenchmarks
     private const string Topic2 = "test1";
     private const string Topic3 = "test2";
     
+    private const string UnusedTopic = "unused";
+    
     [IterationSetup]
     public void Setup()
     {
         _cacheManager = new TopicCacheManager<ushort>(1000, Expiry);
         
         // pre-populate the cache with zero, which is an illegal packetId so it won't get used again
-        for(var i = 0; i < ActiveTopics; i++)
-            _cacheManager.AddItem($"topic{i}", 0);
+        switch (ActiveTopics)
+        {
+            case 1:
+                _cacheManager.AddItem(Topic1, 0);
+                break;
+            case 2:
+                _cacheManager.AddItem(Topic1, 0);
+                _cacheManager.AddItem(Topic2, 0);
+                break;
+            case 3:
+                _cacheManager.AddItem(Topic1, 0);
+                _cacheManager.AddItem(Topic2, 0);
+                _cacheManager.AddItem(Topic3, 0);
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(ActiveTopics));
+        }
     }
     
-    [Benchmark]
-    public void Add()
+    [BenchmarkCategory("Add"), Benchmark(Baseline = true)]
+    public void AddHit()
     {
-        _cacheManager.AddItem(Topic1, 1);
+        _cacheManager.AddItem(Topic1, 0);
     }
     
-    [Benchmark]
+    [BenchmarkCategory("Add"), Benchmark]
+    public void AddMiss()
+    {
+        _cacheManager.AddItem(UnusedTopic, 1);
+    }
+    
+    [BenchmarkCategory("Add"), Benchmark]
+    public void AddMissTopic()
+    {
+        _cacheManager.AddItem(UnusedTopic, 1);
+    }
+    
+    [BenchmarkCategory("Remove"), Benchmark]
     public void Remove()
     {
         _cacheManager.RemoveItem(Topic1, 0);
     }
     
-    [Benchmark]
-    public void Contains()
+    [BenchmarkCategory("Contains"), Benchmark(Baseline = true)]
+    public void ContainsHit()
     {
         _cacheManager.ContainsItem(Topic1, 0);
+    }
+    
+    [BenchmarkCategory("Contains"), Benchmark]
+    public void ContainsMiss()
+    {
+        _cacheManager.ContainsItem(Topic1, 1);
+    }
+    
+    [BenchmarkCategory("Contains"), Benchmark]
+    public void ContainsMissTopic()
+    {
+        _cacheManager.ContainsItem(UnusedTopic, 1);
     }
 }
