b959f8333bd3d4140bb55cb956c6b7426c20d9d8
Aaron Stannard <aaron@petabridge.com>
2024-05-17 16:00:35 -0500

    stubbing out SSL support for MQTT clients

---
+++ src/TurboMqtt/Client/ClientTlsOptions.cs
@@ -0,0 +1,21 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ICertificateProvider.cs" company="Petabridge, LLC">
+//      Copyright (C) 2024 - 2024 Petabridge, LLC <https://petabridge.com>
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System.Net.Security;
+
+namespace TurboMqtt.Client;
+
+/// <summary>
+/// Used to provide the necessary certificates and keys for establishing a secure connection with the MQTT broker.
+/// </summary>
+public sealed record ClientTlsOptions
+{
+    public static readonly ClientTlsOptions Default = new();
+    
+    public bool UseTls { get; init; } = false;
+    
+    public SslClientAuthenticationOptions? SslOptions { get; init; }
+}
--- src/TurboMqtt/Client/MqttClientTcpOptions.cs
+++ src/TurboMqtt/Client/MqttClientTcpOptions.cs
@@ -23,26 +23,33 @@ public sealed record MqttClientTcpOptions
     /// <summary>
     /// Would love to just do IPV6, but that still meets resistance everywhere
     /// </summary>
-    public AddressFamily AddressFamily { get; set; } = AddressFamily.Unspecified;
+    public AddressFamily AddressFamily { get; init; } = AddressFamily.Unspecified;
     
     /// <summary>
     /// Frames are limited to this size in bytes. A frame can contain multiple packets.
     /// </summary>
-    public int MaxFrameSize { get; set; } = 128 * 1024; // 128kb
+    public int MaxFrameSize { get; init; } = 128 * 1024; // 128kb
     
-    public string Host { get; }
+    public string Host { get; init; }
     
-    public int Port { get; }
+    public int Port { get; init; }
     
     /// <summary>
     /// How long should we wait before attempting to reconnect the client?
     /// </summary>
-    public TimeSpan ReconnectInterval { get; set; } = TimeSpan.FromSeconds(5);
+    public TimeSpan ReconnectInterval { get; init; } = TimeSpan.FromSeconds(5);
     
     /// <summary>
     /// Maximum number of times we should attempt to reconnect the client before giving up.
     ///
     /// Resets back to 0 after a successful connection.
     /// </summary>
-    public int MaxReconnectAttempts { get; set; } = 10;
+    public int MaxReconnectAttempts { get; init; } = 10;
+
+    /// <summary>
+    /// The <see cref="ClientTlsOptions"/> to use when connecting to the server.
+    ///
+    /// Disabled by default.
+    /// </summary>
+    public ClientTlsOptions TlsOptions { get; init; } = ClientTlsOptions.Default;
 }
