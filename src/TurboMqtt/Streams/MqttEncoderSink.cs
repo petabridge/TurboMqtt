40b7736e607617fcd23ebcf7d3bc469e5102388b
Aaron Stannard <aaron@petabridge.com>
2024-09-10 17:06:07 -0500

    re-arranged closure code

--- src/TurboMqtt/Streams/MqttEncoderSink.cs
+++ src/TurboMqtt/Streams/MqttEncoderSink.cs
@@ -86,12 +86,15 @@ internal sealed class MqttEncoderSink : GraphStage<SinkShape<List<(MqttPacket pa
 
             _pipeWriter.Advance(totalBytes);
             OnFlushComplete(_pipeWriter.FlushAsync().GetAwaiter().GetResult());
+
+            if (IsClosed(_graphStage.In))
+            {
+                CompleteStage();
+                return;
+            }
             
             if(!HasBeenPulled(_graphStage.In))
                 Pull(_graphStage.In);
-            
-            if(IsClosed(_graphStage.In))
-                CompleteStage();
         }
         
         public override void PreStart()
