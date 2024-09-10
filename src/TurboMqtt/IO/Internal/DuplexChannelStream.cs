53ff70a6515ffeb4cede6b5ef63e49d086a3dce5
Aaron Stannard <aaron@petabridge.com>
2024-09-09 20:22:00 -0500

    wrapping up Channel work

--- src/TurboMqtt/IO/Internal/DuplexChannelStream.cs
+++ src/TurboMqtt/IO/Internal/DuplexChannelStream.cs
@@ -16,17 +16,58 @@ public class DuplexChannelStream : Stream
     private readonly bool _throwOnCancelled;
     private volatile bool _cancelCalled;
 
-    public DuplexChannelStream(ChannelWriter<(IMemoryOwner<byte> buffer, int readableBytes)> output, 
+    public DuplexChannelStream(ChannelWriter<(IMemoryOwner<byte> buffer, int readableBytes)> output,
         ChannelReader<(IMemoryOwner<byte> buffer, int readableBytes)> input, bool throwOnCancelled)
     {
         _output = output;
         _input = input;
         _throwOnCancelled = throwOnCancelled;
     }
-    
+
     public void CancelPendingRead()
     {
         _cancelCalled = true;
     }
-    
+
+    public override void Flush()
+    {
+        // channels don't really have a concept of Flush
+    }
+
+    public override int Read(byte[] buffer, int offset, int count)
+    {
+        
+    }
+
+    public override long Seek(long offset, SeekOrigin origin)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void SetLength(long value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Write(byte[] buffer, int offset, int count)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override ValueTask WriteAsync(ReadOnlyMemory<byte> buffer, CancellationToken cancellationToken = new CancellationToken())
+    {
+        return _output.WriteAsync((new UnsharedMemoryOwner<byte>(buffer), buffer.Length), cancellationToken);
+    }
+
+    public override bool CanRead => true;
+    public override bool CanSeek => false;
+    public override bool CanWrite => true;
+
+    public override long Length => throw new NotSupportedException();
+
+    public override long Position
+    {
+        get => throw new NotSupportedException();
+        set => throw new NotSupportedException();
+    }
 }
